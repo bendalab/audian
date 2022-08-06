ad76aea4a8ea27bd58e65fd21e0069b6bf8d20a2
janscience <jan.benda@uni-tuebingen.de>
2022-08-06 18:42:50 +0200

    simpler runaudian

--- runaudian.py
+++ runaudian.py
@@ -1,5 +1,4 @@
 import audian    # install via pip: pip install audian
 
 filepath = 'feldgr.wav'
-high_pass = 500.0
-audian.main(['-f', f'{high_pass}', filepath])
+audian.main(['-f', '1000', '-l', '15000', filepath])
