f758e8454d6b22253dd6c85d0ef86157fe31744e
Jan Benda <jan.benda@uni-tuebingen.de>
2022-09-27 18:34:16 +0200

    added missing selectviewbox.py

---
+++ audian/selectviewbox.py
@@ -0,0 +1,96 @@
+import numpy as np
+from PyQt5.QtCore import Signal, Qt, QRectF
+from PyQt5.QtGui import QTransform
+import pyqtgraph as pg
+
+
+class SelectViewBox(pg.ViewBox):
+
+
+    sigSelectedRegion = Signal(object, object, object)
+
+
+    def __init__(self, channel, *args, **kwargs):
+        pg.ViewBox.__init__(self, *args, **kwargs)
+        self.channel = channel
+
+
+    def mouseDragEvent(self, ev, axis=None):
+        ## if axis is specified, event will only affect that axis.
+        ev.accept()  ## we accept all buttons
+
+        pos = ev.pos()
+        lastPos = ev.lastPos()
+        dif = pos - lastPos
+        dif = dif * -1
+
+        ## Ignore axes if mouse is disabled
+        mouseEnabled = np.array(self.state['mouseEnabled'], dtype=np.float64)
+        mask = mouseEnabled.copy()
+        if axis is not None:
+            mask[1-axis] = 0.0
+
+        ## Scale or translate based on mouse button
+        if ev.button() in [Qt.MouseButton.LeftButton, Qt.MouseButton.MiddleButton]:
+            if self.state['mouseMode'] == pg.ViewBox.RectMode and axis is None:
+                if ev.isFinish():
+                    # This is the final move in the drag; change the view scale now
+                    rect = QRectF(pg.Point(ev.buttonDownPos(ev.button())), pg.Point(pos))
+                    rect = self.childGroup.mapRectFromParent(rect) # in data coordinates
+                    self.sigSelectedRegion.emit(self.channel, self, rect)
+                else:
+                    ## update shape of scale box
+                    self.updateScaleBox(ev.buttonDownPos(), ev.pos())
+            else:
+                tr = self.childGroup.transform()
+                tr = pg.functions.invertQTransform(tr)
+                tr = tr.map(dif*mask) - tr.map(pg.Point(0,0))
+
+                x = tr.x() if mask[0] == 1 else None
+                y = tr.y() if mask[1] == 1 else None
+
+                self._resetTarget()
+                if x is not None or y is not None:
+                    self.translateBy(x=x, y=y)
+                self.sigRangeChangedManually.emit(self.state['mouseEnabled'])
+        elif ev.button() & Qt.MouseButton.RightButton:
+            #print "vb.rightDrag"
+            if self.state['aspectLocked'] is not False:
+                mask[0] = 0
+
+            dif = ev.screenPos() - ev.lastScreenPos()
+            dif = np.array([dif.x(), dif.y()])
+            dif[0] *= -1
+            s = ((mask * 0.02) + 1) ** dif
+
+            tr = self.childGroup.transform()
+            tr = pg.functions.invertQTransform(tr)
+
+            x = s[0] if mouseEnabled[0] == 1 else None
+            y = s[1] if mouseEnabled[1] == 1 else None
+
+            center = pg.Point(tr.map(ev.buttonDownPos(Qt.MouseButton.RightButton)))
+            self._resetTarget()
+            self.scaleBy(x=x, y=y, center=center)
+            self.sigRangeChangedManually.emit(self.state['mouseEnabled'])
+
+
+    def updateScaleBox(self, p1, p2):
+        r = QRectF(p1, p2)
+        r = self.childGroup.mapRectFromParent(r)
+        self.rbScaleBox.setPos(r.topLeft())
+        tr = QTransform.fromScale(r.width(), r.height())
+        self.rbScaleBox.setTransform(tr)
+        self.rbScaleBox.show()
+
+        
+    def hide_region(self):
+        self.rbScaleBox.hide()
+
+        
+    def zoom_region(self, rect):
+        self.hide_region()
+        self.showAxRect(rect)
+        self.axHistoryPointer += 1
+        self.axHistory = self.axHistory[:self.axHistoryPointer] + [rect]
+
